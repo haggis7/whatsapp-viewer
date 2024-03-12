accd1900ca1660dd64010e3fd4ba32bf309bb27a
haggis7 <116017779+haggis7@users.noreply.github.com>
2024-03-12 22:26:41 +0800

    Update Crypt14.cpp

--- source/WhatsApp/Crypt14.cpp
+++ source/WhatsApp/Crypt14.cpp
@@ -7,5 +7,6 @@ void decryptWhatsappDatabase14(const std::string& filename, const std::string& f
 	unsigned char iv[16];
 
 	loadKey(keyFilename, key, iv);
-	decryptWhatsappDatabase12_14(filename, filenameDecrypted, key, 67, 191, 0);
-}
+	decryptWhatsappDatabase12_14(filename, filenameDecrypted, key, 67, 191, 0)
+(
+	msgstore-2022-09-19.1.db.crypt14,key, 67,191,0)
